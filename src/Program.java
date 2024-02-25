396d3d8a628f53fce9b4a293735ea8d8f1f52c43
Никита Фомин <defdXs@yandex.ru>
2024-02-25 17:16:10 +0300

    fffff

--- src/Program.java
+++ src/Program.java
@@ -4,33 +4,31 @@ import Core.LabCore.LabManager;
 
 public class Program {
     public static void main(String[] args) throws Exception {
-        if (args.length > 0) {
-            switch (args.length) {
-                case 1:
-                    LabManager.getInstance().run(
-                        Integer.parseInt(args[0])
-                    );
-                    break;
+        switch (args.length) {
+            case 0:
+                LabManager.getInstance().run(2);
+                break;
 
-                case 2:
-                    LabManager.getInstance().run(
-                        Integer.parseInt(args[0]),
-                        Integer.parseInt(args[1])
-                    );
-                    break;
+            case 1:
+                LabManager.getInstance().run(
+                    Integer.parseInt(args[0])
+                );
+                break;
 
-                default:
-                    LabManager.getInstance().run(
-                        Integer.parseInt(args[0]),
-                        Integer.parseInt(args[1]),
-                        Arrays.copyOfRange(args, 2, args.length)
-                    );
-                    break;
-            }
-                
-        }
-        else {
-            LabManager.getInstance().run(2);
+            case 2:
+                LabManager.getInstance().run(
+                    Integer.parseInt(args[0]),
+                    Integer.parseInt(args[1])
+                );
+                break;
+
+            default:
+                LabManager.getInstance().run(
+                    Integer.parseInt(args[0]),
+                    Integer.parseInt(args[1]),
+                    Arrays.copyOfRange(args, 2, args.length)
+                );
+                break;
         }
     }
 }
