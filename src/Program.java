77398cf8c86ffad623994758225dbbd4d0d62d15
Никита Фомин <defdXs@yandex.ru>
2024-02-25 18:50:21 +0300

    eeeeeee

--- src/Program.java
+++ src/Program.java
@@ -6,11 +6,21 @@ import Core.LabCore.LabManager;
 public class Program {
     public static void main(String[] args) throws Exception {
         cls();
-        switch (args.length) {
-            case 0:
-                LabManager.getInstance().run(2);
-                break;
+        if (args.length != 0) exec(args);
+        else exec(2);
+    }
 
+    private static void exec(int ilab) throws Exception {
+        LabManager.getInstance().run(ilab);
+    }
+    private static void exec(int ilab, int itask) throws Exception {
+        LabManager.getInstance().run(ilab, itask);
+    }
+    private static void exec(int ilab, int itask, String[] args) throws Exception {
+        LabManager.getInstance().run(ilab, itask, args);
+    }
+    private static void exec(String[] args) throws Exception {
+        switch (args.length) {
             case 1:
                 LabManager.getInstance().run(
                     Integer.parseInt(args[0])
