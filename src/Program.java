2b1924e4aa78fac61804894ca74150e10c9cebe8
Никита Фомин <defdXs@yandex.ru>
2024-03-10 14:02:33 +0300

    Runned Lab3

--- src/Program.java
+++ src/Program.java
@@ -5,6 +5,6 @@ public class Program {
     public static void main(String[] args) throws Exception {
         cls();
         if (args.length != 0) exec(args);
-        else exec(2, 0, new String[]{"500", "50", "10", "10", "133", "abcd"});
+        else exec(3);
     }
 }
