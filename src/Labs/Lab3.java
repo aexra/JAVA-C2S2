930e3e326a6ea560bf679a4da4bf2b0436e4976a
Никита Фомин <defdXs@yandex.ru>
2024-03-10 16:20:03 +0300

    l3t1 done

--- src/Labs/Lab3.java
+++ src/Labs/Lab3.java
@@ -18,7 +18,14 @@ public class Lab3 extends Lab {
 
     @ILabTask(description = "Таблица значений sin(x) и e^x / x * lg(x) из интервала [pi/15..pi] с шагом pi/15")
     public void t1() {
-
+        System.out.println("Таблица значений функций");
+        System.out.printf("%-10s| %-15s| %-15s\n", "x", "sin(x)", "e^x / x * lg(x)");
+        double val1, val2;
+        for (double arg = Math.PI/15; arg < Math.PI; arg += Math.PI/15) {
+            val1 = Math.sin(arg);
+            val2 = Math.exp(arg) / arg * Math.log(arg);
+            System.out.printf("%-10.5f| %-15.7f| %-15.7f\n", arg, val1, val2);
+        }
     }
 
     @ILabTask(description = "Поиск наибольшего отрицательного элемента двумерного массива")
