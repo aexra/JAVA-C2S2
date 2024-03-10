ceceb96ea852c20051e1ece22abed85fad95ab38
Никита Фомин <defdXs@yandex.ru>
2024-03-10 14:01:40 +0300

    Created Lab3 class

---
+++ src/Labs/Lab3.java
@@ -0,0 +1,7 @@
+package Labs;
+
+import Core.LabManagement.Lab;
+
+public class Lab3 extends Lab {
+    
+}
--- src/Program.java
+++ src/Program.java
@@ -1,47 +1,10 @@
 import static Helpers.ConsoleHelper.cls;
-
-import java.util.Arrays;
-
-import Core.LabManagement.LabManager;
+import static Helpers.LabExecutors.*;
 
 public class Program {
     public static void main(String[] args) throws Exception {
         cls();
         if (args.length != 0) exec(args);
         else exec(2, 0, new String[]{"500", "50", "10", "10", "133", "abcd"});
-    }
-
-    private static void exec(int ilab) throws Exception {
-        LabManager.getInstance().run(ilab);
-    }
-    private static void exec(int ilab, int itask) throws Exception {
-        LabManager.getInstance().run(ilab, itask);
-    }
-    private static void exec(int ilab, int itask, String[] args) throws Exception {
-        LabManager.getInstance().run(ilab, itask, args);
-    }
-    private static void exec(String[] args) throws Exception {
-        switch (args.length) {
-            case 1:
-                LabManager.getInstance().run(
-                    Integer.parseInt(args[0])
-                );
-                break;
-
-            case 2:
-                LabManager.getInstance().run(
-                    Integer.parseInt(args[0]),
-                    Integer.parseInt(args[1])
-                );
-                break;
-
-            default:
-                LabManager.getInstance().run(
-                    Integer.parseInt(args[0]),
-                    Integer.parseInt(args[1]),
-                    Arrays.copyOfRange(args, 2, args.length)
-                );
-                break;
-        }
     }
 }
