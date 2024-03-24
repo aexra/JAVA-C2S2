7444f07266b081e2afa745bbb31253b40d05b61a
Никита Фомин <defdXs@yandex.ru>
2024-03-24 17:19:55 +0300

    much more methods

--- src/Labs/Classes/Complex.java
+++ src/Labs/Classes/Complex.java
@@ -1,9 +1,11 @@
 package Labs.Classes;
 
+import java.text.MessageFormat;
+
 public class Complex {
     // FIELDS
-    private double real;
-    private double imagine;
+    private double real = 0;
+    private double imagine = 0;
 
     // CONSTRUCTORS
     public Complex(double _real, double _imagine) {
@@ -11,7 +13,7 @@ public class Complex {
         this.imagine = _imagine;
     }
 
-    // REAL SETTER
+    // REAL GETTER & SETTER
     public double getReal() {
         return this.real;
     }
@@ -20,7 +22,7 @@ public class Complex {
         return this.real;
     }
 
-    // IMAGINE SETTER
+    // IMAGINE GETTER & SETTER
     public double getImagine() {
         return this.imagine;
     }
@@ -29,7 +31,27 @@ public class Complex {
         return this.imagine;
     }
 
+    // ARGUMENT GETTER
+    public double getArg() {
+        return Math.atan2(this.imagine, this.real);
+    }
+
+    // MODULE GETTER
+    public double getModule() {
+        return Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imagine, 2));
+    }
+
     // OPERATIONS
+    // ==
+    public static boolean equal(Complex _left, Complex _right) {
+        return _left.getReal() == _right.getReal() && _left.getImagine() == _right.getImagine();
+    }
+    public static boolean equal(double _left, Complex _right) {
+        return _left == _right.getReal() && _right.getImagine() == 0;
+    }
+    public static boolean equal(Complex _left, double _right) {
+        return _left.getReal() == _right && _left.getImagine() == 0;
+    }
     // +
     public static Complex concat(Complex _left, Complex _right) {
         return new Complex(_left.getReal() + _right.getReal(), _left.getImagine() + _right.getImagine());
@@ -88,6 +110,13 @@ public class Complex {
             _right * _left.getImagine() / Math.pow(_right, 2)
         );
     }
+    // ===
+    public boolean equal(Complex _right) {
+        return Complex.equal(this, _right);
+    }
+    public boolean equal(double _right) {
+        return Complex.equal(this, _right);
+    }
     // +=
     public Complex add(Complex _right) {
         var z = Complex.concat(this, _right);
@@ -139,5 +168,14 @@ public class Complex {
         this.real = z.getReal();
         this.imagine = z.getImagine();
         return this;
+    }
+
+    // OTHER METHODS
+    public String toString() {
+        return MessageFormat.format("{0} + i*{1}", this.real, this.imagine);
+    }
+    public String toString(boolean T) {
+        if (!T) return this.toString();
+        else return MessageFormat.format("{0}*(cos({1})+i*sin({1}))", getModule(), getArg());
     }
 }
