a9f957d509c355d668fc04edbff41c45d233caff
Никита Фомин <defdXs@yandex.ru>
2024-03-24 22:23:50 +0300

    Created autobaza class

---
+++ src/Labs/Classes/Autobaza.java
@@ -0,0 +1,10 @@
+package Labs.Classes;
+
+public final class Autobaza {
+    private Car[] cars;
+
+    public Autobaza(int size) throws Exception {
+        if (size < 1) throw new Exception("Autobaza must be at least size 1");
+        
+    }
+}
