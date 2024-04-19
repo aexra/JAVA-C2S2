527eef67a499fc671811f56750fdf1da65f08b63
Никита Фомин <defdXs@yandex.ru>
2024-04-19 11:32:51 +0300

    Help

--- src/Labs/Lab5.java
+++ src/Labs/Lab5.java
@@ -4,6 +4,7 @@ import Core.Interfaces.ILabTask;
 import Core.LabManagement.Lab;
 import Labs.Data5.Classes.*;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import static Helpers.Logger.*;
@@ -86,5 +87,75 @@ public class Lab5 extends Lab {
             } else
                 System.out.println("Ошибка! Длина должна быть целым числом!");
         }
+    }
+
+    @ILabTask(description = "coins")
+    public void t8() {
+        int sum = 11;
+        ArrayList<Integer> coins = new ArrayList<>();
+        coins.add(1);
+        coins.add(2);
+        coins.add(5);
+        coins.add(7);
+        print(change(sum, coins));
+        change(sum, coins);
+    }
+
+    static void print(Pair<Integer, ArrayList<ArrayList<Integer>>> answer) {
+        System.out.format("Минимальное число монет для выдачи сдачи: %d\n", answer.left);
+        System.out.println("Оптимальные наборы монет:");
+        for (int i = 0; i < answer.right.size(); i++) {
+            System.out.print("[");
+            for (int j = 0; j < answer.right.get(i).size(); j++) {
+                System.out.format("%d, ", answer.right.get(i).get(j));
+            }
+            System.out.println("]");
+        }
+    }
+
+    static Pair<Integer, ArrayList<ArrayList<Integer>>> change(int sum, ArrayList<Integer> coins) {
+        DList<Integer, ArrayList<Integer>> dList = new DList<>();
+
+        ArrayList<ArrayList<Integer>> empty_array = new ArrayList<>();
+        empty_array.add(new ArrayList<>());
+        dList.add(0, empty_array);
+        for (int i = 1; i <= sum; i++) {
+            dList.add(sum + 1, new ArrayList<ArrayList<Integer>>());
+        }
+
+        for (int i = 1; i <= sum; i++) {
+            for (int j = 0; j < coins.size(); j++) {
+                if (coins.get(j) <= i) {
+
+                    if (dList.getAt(i).left < 1 + dList.getAt(i - coins.get(j)).left) {
+                        continue;
+                    } else if (dList.getAt(i).left > 1 + dList.getAt(i - coins.get(j)).left) {
+                        int current_coins = 1 + dList.getAt(i - coins.get(j)).left;
+                        ArrayList<ArrayList<Integer>> array = new ArrayList<>();
+                        for (int k = 0; k < dList.getAt(i - coins.get(j)).right.size(); k++) {
+                            array.add(new ArrayList<>());
+                            array.get(k).addAll(dList.getAt(i - coins.get(j)).right.get(k));
+                        }
+                        for (int k = 0; k < array.size(); k++) {
+                            array.get(k).add(coins.get(j));
+                        }
+                        dList.setAt(new Pair<>(current_coins, array), i);
+                    } else {
+                        ArrayList<ArrayList<Integer>> array = new ArrayList<>();
+                        for (int k = 0; k < dList.getAt(i - coins.get(j)).right.size(); k++) {
+                            array.add(new ArrayList<>());
+                            array.get(k).addAll(dList.getAt(i - coins.get(j)).right.get(k));
+                        }
+                        for (int k = 0; k < array.size(); k++) {
+                            array.get(k).add(coins.get(j));
+                        }
+                        array.addAll(dList.getAt(i).right);
+                        dList.setAt(new Pair<>(dList.getAt(i).left, array), i);
+                    }
+                }
+            }
+        }
+
+        return dList.getAt(sum);
     }
 }
